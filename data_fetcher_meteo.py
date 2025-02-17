8c89c653943df175c107e6f6f46118a06a2b28a9
Elias Dargham <edargham@hotmail.com>
2025-02-18 01:16:34 +0200

    18/08/2025 - Update data fetching for Beirut: adjust date range, include hourly data, and modify CSV output format

--- data_fetcher_meteo.py
+++ data_fetcher_meteo.py
@@ -1,18 +1,23 @@
 from datetime import datetime
-from meteostat import Point, Daily
+from meteostat import Point, Daily, Hourly
 import pandas as pd
 
 # Set time period
-start = datetime(2000, 1, 1)
-end = datetime(2024, 12, 31)
+start = datetime(2000, 6, 1)
+end = datetime(2024, 6, 2)
 
 # Create Point for Beirut
-beirut = Point(33.8938, 35.5018, 10)
+beirut = Point(33.8938, 35.5018)
+beirut.radius = 30000
 
 # Get daily data
 data = Daily(beirut, start, end)
 data = data.fetch()
 
+# Keep only prcp column
+data = data[['prcp']]
+
 # Save data to CSV
-data.to_csv('data/beirut.csv', index=False)
+data.to_csv('data/beirut.csv', index=True)
 print(data.tail())
+print(data['prcp'].sum())
