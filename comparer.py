4c6550f13e25d01f55827d88a21cdc31532fb000
Elias Dargham <edargham@hotmail.com>
2025-03-22 18:51:50 +0200

    Remove xgboost.py and update unit conversion script to calculate daily temperature statistics

--- comparer.py
+++ comparer.py
@@ -23,8 +23,8 @@ def nash_sutcliffe_efficiency(observed, simulated):
   return 1 - (numerator / denominator) if denominator != 0 else np.nan
 
 # Load CSV files from the folder "data"
-accumulated_path = "data/beirut-ndvi-rhia-8day-landsat-2.csv"
-meteostat_path = "data/40100.csv"
+accumulated_path = "data/fldas-soil-moist.csv"
+meteostat_path = "data/40100-refined.csv"
 
 
 df_accumulated = pd.read_csv(accumulated_path)
@@ -46,7 +46,7 @@ highest_correlation = -1
 highest_inverse_correlation = 0
 
 # Iterate over a range of shift days to find the best shift
-for shift_days in range(-90, 0):  # Example range from -30 to 30 days
+for shift_days in range(-90, 90):  # Example range from -30 to 30 days
   shifted_df_accumulated = df_accumulated.copy()
   shifted_df_accumulated['date'] = shifted_df_accumulated['date'] + pd.DateOffset(days=shift_days)
   
@@ -55,7 +55,8 @@ for shift_days in range(-90, 0):  # Example range from -30 to 30 days
   
   # Drop NaN rows
   merged_df = merged_df.dropna()
-  
+  merged_df = merged_df[(merged_df['value_met'] > 0)]
+
   # Compute correlation coefficient
   correlation = merged_df['value_acc'].corr(merged_df['value_met'])
   
@@ -78,95 +79,95 @@ print(f"Best inverse shift: {best_inverse_shift} days")
 print(f"Highest inverse correlation: {highest_inverse_correlation}")
 
 
-# Select rows from both datasets where the date column matches and both "value" columns are non-zero
-merged_df = pd.merge(df_accumulated, df_meteostat, on='date', suffixes=('_acc', '_met'))
-#merged_df = merged_df[(merged_df['value_met'] > 0)]
-print(merged_df.head())
+# # Select rows from both datasets where the date column matches and both "value" columns are non-zero
+# merged_df = pd.merge(df_accumulated, df_meteostat, on='date', suffixes=('_acc', '_met'))
+# #merged_df = merged_df[(merged_df['value_met'] > 0)]
+# print(merged_df.head())
 
-# Drop NaN rows
-merged_df = merged_df.dropna()
+# # Drop NaN rows
+# merged_df = merged_df.dropna()
 
-# merged_df['value_met'] = MinMaxScaler().fit_transform(merged_df[['value_met']])
+# # merged_df['value_met'] = MinMaxScaler().fit_transform(merged_df[['value_met']])
 
-merged_df['bias_coef'] = merged_df['value_acc'] / (merged_df['value_met'] + 1e-4)
-merged_df['diff'] = merged_df['value_acc'] - merged_df['value_met']
+# merged_df['bias_coef'] = merged_df['value_acc'] / (merged_df['value_met'] + 1e-4)
+# merged_df['diff'] = merged_df['value_acc'] - merged_df['value_met']
 
-merged_df.to_csv('data/comp-merged.csv', index=False)
+# merged_df.to_csv('data/comp-merged.csv', index=False)
 
-df_accumulated = merged_df[['date', 'value_acc']].rename(columns={'value_acc': 'value'})
-df_meteostat = merged_df[['date', 'value_met']].rename(columns={'value_met': 'value'})
+# df_accumulated = merged_df[['date', 'value_acc']].rename(columns={'value_acc': 'value'})
+# df_meteostat = merged_df[['date', 'value_met']].rename(columns={'value_met': 'value'})
 
-# To compare using RMSE, we need to ensure the two arrays align.
-# For simplicity, we'll assume they align by index. If necessary, you can merge them on a common key.
+# # To compare using RMSE, we need to ensure the two arrays align.
+# # For simplicity, we'll assume they align by index. If necessary, you can merge them on a common key.
 
-# Align indices by taking the intersection of indices if lengths differ
-common_index = df_accumulated.index.intersection(df_meteostat.index)
-values_accumulated = merged_df[['date', 'value_acc']]
-values_accumulated['value'] = values_accumulated['value_acc']
-values_accumulated = values_accumulated.drop(columns='value_acc')
-print(values_accumulated)
-values_accumulated.to_csv('data/comp-accumulated.csv', index=False)
+# # Align indices by taking the intersection of indices if lengths differ
+# common_index = df_accumulated.index.intersection(df_meteostat.index)
+# values_accumulated = merged_df[['date', 'value_acc']]
+# values_accumulated['value'] = values_accumulated['value_acc']
+# values_accumulated = values_accumulated.drop(columns='value_acc')
+# print(values_accumulated)
+# values_accumulated.to_csv('data/comp-accumulated.csv', index=False)
 
-values_meteostat = merged_df[['date', 'value_met']]
-values_meteostat['value'] = values_meteostat['value_met']
-values_meteostat = values_meteostat.drop(columns='value_met')
-print(values_meteostat)
-values_meteostat.to_csv('data/comp-meteostat.csv', index=False)
+# values_meteostat = merged_df[['date', 'value_met']]
+# values_meteostat['value'] = values_meteostat['value_met']
+# values_meteostat = values_meteostat.drop(columns='value_met')
+# print(values_meteostat)
+# values_meteostat.to_csv('data/comp-meteostat.csv', index=False)
 
-# Compute RMSE
-mse = np.mean((values_accumulated['value'] - values_meteostat['value']) ** 2)
-rmse = sqrt(mse)
-print("MSE:", mse)
-print("RMSE:", rmse)
+# # Compute RMSE
+# mse = np.mean((values_accumulated['value'] - values_meteostat['value']) ** 2)
+# rmse = sqrt(mse)
+# print("MSE:", mse)
+# print("RMSE:", rmse)
 
-# Compare the sum of the values
-sum_accumulated = values_accumulated['value'].sum()
-sum_meteostat = values_meteostat['value'].sum()
-print("Sum of accumulated values:", sum_accumulated)
-print("Sum of meteostat values:", sum_meteostat)
-print("Difference in sums:", abs(sum_accumulated - sum_meteostat))
+# # Compare the sum of the values
+# sum_accumulated = values_accumulated['value'].sum()
+# sum_meteostat = values_meteostat['value'].sum()
+# print("Sum of accumulated values:", sum_accumulated)
+# print("Sum of meteostat values:", sum_meteostat)
+# print("Difference in sums:", abs(sum_accumulated - sum_meteostat))
 
-# Compute R2 score
-r2 = r2_score(
-  values_meteostat['value'],
-  values_accumulated['value'],
-)
-print("R2 score:", r2)
+# # Compute R2 score
+# r2 = r2_score(
+#   values_meteostat['value'],
+#   values_accumulated['value'],
+# )
+# print("R2 score:", r2)
 
-# Compute correlation coefficient
-correlation = values_accumulated['value'].corr(values_meteostat['value'])
-print("Correlation coefficient:", correlation)
+# # Compute correlation coefficient
+# correlation = values_accumulated['value'].corr(values_meteostat['value'])
+# print("Correlation coefficient:", correlation)
 
-# Compute MAE
-mae = np.mean(np.abs(values_accumulated['value'] - values_meteostat['value']))
-print("MAE:", mae)
+# # Compute MAE
+# mae = np.mean(np.abs(values_accumulated['value'] - values_meteostat['value']))
+# print("MAE:", mae)
 
-# Compute NSE
-nse = nash_sutcliffe_efficiency(
-  values_accumulated['value'],
-  values_meteostat['value'],
-)
-print("NSE:", nse)
+# # Compute NSE
+# nse = nash_sutcliffe_efficiency(
+#   values_accumulated['value'],
+#   values_meteostat['value'],
+# )
+# print("NSE:", nse)
 
-print("\n -- More Stats -- \n")
+# print("\n -- More Stats -- \n")
 
-# Calculate standard deviation of each dataset
-std_acc = merged_df['value_acc'].std()
-std_met = merged_df['value_met'].std()
+# # Calculate standard deviation of each dataset
+# std_acc = merged_df['value_acc'].std()
+# std_met = merged_df['value_met'].std()
 
-mean_acc = merged_df['value_acc'].mean()
-mean_met = merged_df['value_met'].mean()
+# mean_acc = merged_df['value_acc'].mean()
+# mean_met = merged_df['value_met'].mean()
 
-# Perform a paired t-test (if data is normally distributed) or Wilcoxon signed-rank test
-# Here, we use Wilcoxon due to potential non-normality
-stat, p = stats.wilcoxon(merged_df['value_met'], merged_df['value_acc'])
+# # Perform a paired t-test (if data is normally distributed) or Wilcoxon signed-rank test
+# # Here, we use Wilcoxon due to potential non-normality
+# stat, p = stats.wilcoxon(merged_df['value_met'], merged_df['value_acc'])
 
-print(f"Mean of value_acc: {mean_acc}, value_met: {mean_met}")
-print(f"Standard Deviation of value_acc: {std_acc}, value_met: {std_met}")
-print(f"Wilcoxon Test Statistic: {stat}, p-value: {p}")
+# print(f"Mean of value_acc: {mean_acc}, value_met: {mean_met}")
+# print(f"Standard Deviation of value_acc: {std_acc}, value_met: {std_met}")
+# print(f"Wilcoxon Test Statistic: {stat}, p-value: {p}")
 
-# Interpret p-value
-if p < 0.05:
-    print("The difference is statistically significant.")
-else:
-    print("The difference is not statistically significant.")
+# # Interpret p-value
+# if p < 0.05:
+#     print("The difference is statistically significant.")
+# else:
+#     print("The difference is not statistically significant.")
--- data-concat.py
+++ data-concat.py
@@ -7,18 +7,28 @@ folder_path = 'data'
 
 # List of CSV files to read
 csv_files = [
-  ('40100.csv', 'meteostat'),
-  ('beirut-lst-day-rhia.csv', 'lst-day'),
-  ('beirut-lst-night-rhia.csv', 'lst-night'),
-  ('beirut-ndvi-rhia-daily.csv', 'ndvi'),
-  ('beirut-ndwi-rhia-daily.csv', 'ndwi'),
-  ('gpm-bey.csv', 'gpm')
+  ('40100-refined.csv', 'meteostat'),
+  # ('beirut-lst-day-rhia.csv', 'lst-day'),
+  # ('beirut-lst-night-rhia.csv', 'lst-night'),
+  # ('beirut-daily-temps-max.csv', 'temperature-max'),
+  # ('beirut-daily-temps-min.csv', 'temperature-min'),
+  # ('beirut-daily-temps-mean.csv', 'temperature-mean'),
+  # ('beirut-ndvi-rhia-daily.csv', 'ndvi'),
+  # ('beirut-ndwi-rhia-daily.csv', 'ndwi'),
+  # ('soil-m.csv', 'soil-moisture'),
+  # ('soil-t.csv', 'soil-temperature'),
+  #('gpm-bey.csv', 'gpm-precipitation'),
+  ('fldas-evap.csv', 'evapotranspiration'),
+  ('fldas-runoff.csv', 'runoff'),
+  # ('airs-bey.csv', 'airs-precipitation'),
 ]
 
-# csv_files_shift_date = [
-#   ('beirut-landsat-combined-ndwi.csv', 'landsat-ndwi', -42),
-#   ('beirut-landsat-combined-ndvi.csv', 'landsat-ndvi', -63)
-# ]
+csv_files_shift_date = [
+  ('fldas-radio-temp-max.csv', 'radio-temp-min', 0),
+  ('fldas-sat.csv', 'sat', -1),
+  ('fldas-soil-moist.csv', 'soil-moist', -1),
+  ('fldas-soil-temp.csv', 'soil-temp', 0),
+]
 
 # Initialize a list to store dataframes
 dfs = []
@@ -46,30 +56,30 @@ for file in csv_files:
 
 
 # Read each CSV file with date shift
-# for file in csv_files_shift_date:
-#   file_path = os.path.join(folder_path, file[0])
-#   df = pd.read_csv(file_path)
-#   df.drop(columns=['system:index', '.geo'], inplace=True)
+for file in csv_files_shift_date:
+  file_path = os.path.join(folder_path, file[0])
+  df = pd.read_csv(file_path)
+  # df.drop(columns=['system:index', '.geo'], inplace=True)
   
-#   # Convert date column to datetime
-#   if 'date' in df.columns:
-#     df['date'] = pd.to_datetime(df['date'])
+  # Convert date column to datetime
+  if 'date' in df.columns:
+    df['date'] = pd.to_datetime(df['date'])
     
-#     # Get a prefix from the filename (without extension)
-#     prefix = file[1]
+    # Get a prefix from the filename (without extension)
+    prefix = file[1]
     
-#     # Rename all columns except 'date' to include the prefix
-#     df.columns = [col if col == 'date' else f"{prefix}_{col}" for col in df.columns]
+    # Rename all columns except 'date' to include the prefix
+    df.columns = [col if col == 'date' else f"{prefix}_{col}" for col in df.columns]
     
-#     # Shift the date column by a number of days
-#     shift_days = file[2]
-#     df['date'] = df['date'] + pd.DateOffset(days=shift_days)
-#   else:
-#     print(f"Warning: {file} does not have a 'date' column")
-#     continue
+    # Shift the date column by a number of days
+    shift_days = file[2]
+    df['date'] = df['date'] + pd.DateOffset(days=shift_days)
+  else:
+    print(f"Warning: {file} does not have a 'date' column")
+    continue
   
-#   dfs.append(df)
-#   print(f"Read {file}: {df.shape[0]} rows, {df.shape[1]} columns")
+  dfs.append(df)
+  print(f"Read {file}: {df.shape[0]} rows, {df.shape[1]} columns")
 
 # Merge all dataframes on the date column
 if dfs:
@@ -83,8 +93,13 @@ if dfs:
 
   # Fill missing values with NaN
   merged_df = merged_df.dropna()
+ 
+  # Drop 50% of the rows where the target variable is zero
+  meteostat_value = 'meteostat_value'
+  zero_rows = merged_df[merged_df[meteostat_value] == 0].index
+  drop_indices = np.random.choice(zero_rows, int(len(zero_rows) * 0.0), replace=False)
+  merged_df = merged_df.drop(drop_indices)
 
-  
   # Save the merged dataframe
   merged_df.to_csv('merged_data.csv', index=False)
   
